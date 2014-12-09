08979f4c1f2b6f6ab82508205397f2aacd036830
Tobias Klika <cee@live.at>
2014-12-09 23:06:16 +0100

    start tests

---
+++ FeedlySharp.Tests/TestsBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FeedlySharp.Tests
+{
+  public class TestsBase : IDisposable
+  {
+    protected FeedlyClient client;
+
+    // setup
+    public TestsBase()
+    {
+      
+    }
+
+
+    // teardown
+    public void Dispose()
+    {
+      
+    }
+
+
+    // async throws
+    public static async Task ThrowsAsync<TException>(Func<Task> func)
+    {
+      var expected = typeof(TException);
+      Type actual = null;
+      try
+      {
+        await func();
+      }
+      catch (Exception e)
+      {
+        actual = e.GetType();
+      }
+      Assert.Equal(expected, actual);
+    }
+  }
+}
