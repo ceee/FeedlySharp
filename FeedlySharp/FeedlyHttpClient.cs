e55c43cdf3e61185f97ec2ace96d39ac1c94e0d4
Tobias Klika <cee@live.at>
2019-02-27 15:40:49 +0100

    migrate to .NET Standard 2

--- FeedlySharp/FeedlyHttpClient.cs
+++ FeedlySharp/FeedlyHttpClient.cs
@@ -1,13 +1,11 @@
-﻿using FeedlySharp.Extensions;
+using FeedlySharp.Extensions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
